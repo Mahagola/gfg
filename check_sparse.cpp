d0ddbe2e5ceaa2c67ba9128ed0135ee22d726723
Suryansh <104053578+Mahagola@users.noreply.github.com>
2023-06-22 20:13:21 +0530

    Update check_sparse.cpp

--- check_sparse.cpp
+++ check_sparse.cpp
@@ -1,12 +1,14 @@
+class Solution
+{
+    public:
+    //Function to check if the number is sparse or not.
     bool isSparse(int n)
     {
-        // Your code
         int count=0;
         int temp=1;
         while(n>0&&count!=2){
             if((n&temp)==1) {
                 count++;
-                temp<<=1;
             }
             else{
             count=0;
@@ -20,3 +22,4 @@
             return true;
         }
     }
+};
